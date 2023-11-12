71418c8b671e96d2e852cc1d631fe424a6b1ccef
Yehyun <yehyun.suh@vanderbilt.edu>
2023-11-12 16:16:15 +0000

    Save test result in csv

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import torch
 import math
 import time
 import os 
+import csv
 import numpy as np
 
 from dataset import load_data
@@ -20,6 +21,7 @@ def test(args, model, DEVICE):
     model.eval()
     dice_score, rmse_total = 0, 0
     extracted_pixels_list = []
+    extracted_pixels_to_df = []
     rmse_list = [[0]*len(test_loader) for _ in range(args.output_channel)]
     angle_list = [[0]*len(test_loader) for _ in range(len(args.label_for_angle))]
     angle_total = []
@@ -43,6 +45,12 @@ def test(args, model, DEVICE):
             rmse_list, index_list = rmse(args, prediction, label_list, idx, rmse_list)
             extracted_pixels_list.append(index_list)
 
+            extracted_pixels_array = np.array(index_list[0]).reshape(-1)
+            tmp_list = [f'{image_name}.png', len(index_list[0])]
+            for i in range(len(extracted_pixels_array)):
+                tmp_list.append(extracted_pixels_array[i])
+            extracted_pixels_to_df.append(tmp_list)
+
             ## make predictions to be 0. or 1.
             prediction_binary = (prediction > 0.5).float()
 
@@ -52,9 +60,19 @@ def test(args, model, DEVICE):
                 predict_spatial_mean, label_spatial_mean, None, 'test'
             )
         end = time.time()
-    
-    log_terminal(args, "test_prediction", extracted_pixels_list)
-    log_terminal(args, "test_label", label_total)
-    
+
     print("=====Testing Process Done=====")
     print(f"{end - start:.5f} seconds for {len(test_loader)} images")
+
+    log_terminal(args, "test_prediction", extracted_pixels_list)
+    log_terminal(args, "test_label", label_total)
+
+    row_name = ["image_name", "number_of_labels"]
+    for i in range(args.output_channel):
+        row_name.append(f'label_{i}_y')
+        row_name.append(f'label_{i}_x')
+    csv_path = f'results/{args.wandb_name}/{args.wandb_name}_test_prediction.csv'
+    with open(csv_path, 'w', newline='') as f:
+        write = csv.writer(f)
+        write.writerow(row_name)
+        write.writerows(sorted(extracted_pixels_to_df))
